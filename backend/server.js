76ddc38a1a2a3e250b1c64010f5a74f2366f8d4a
u-khang <khang.nguyen8675@gmail.com>
2025-09-04 13:01:58 -0400

    maybe fix CORS configuration to allow cache-control headers

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(cors({
   origin: config.ALLOWED_ORIGINS,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control', 'Pragma'],
   optionsSuccessStatus: 200
 }));
 app.use(express.json());
